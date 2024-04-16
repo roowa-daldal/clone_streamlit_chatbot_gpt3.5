91c5ac9a3a8fe8aedcc87a74b4596a3de4bcbcd6
CodeMath <xyrho123@naver.com>
2024-04-17 01:12:09 +0900

    Feat : Streamlit 기능 테스트

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 import streamlit as st
-
-st.title("Echo Bot")
+import time
+st.title("G-Bot")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -12,15 +12,30 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # React to user input
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("무엇이든 물어보세요!"):
     # Display user message in chat message container
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f"Echo: {prompt}"
+    response = f"AI 서칭 결과: [{prompt}](https://www.google.com/search?q={prompt})"
     # Display assistant response in chat message container
+
+    with st.status("Wait for response...", expanded=True) as status:
+
+        st.write("1. Ask AI & RAG engine...")
+        time.sleep(1)
+        st.write("2. Get response...")
+        time.sleep(1)
+        st.write("3. Now make a response for you!")
+        time.sleep(1)
+        status.update(label="Response!", state="complete", expanded=False)
+    st.success('Done!')
     with st.chat_message("assistant"):
         st.markdown(response)
+        st.toast('Hooray!', icon='🎉')
+        st.balloons()
+    time.sleep(2)
+    st.snow()
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
